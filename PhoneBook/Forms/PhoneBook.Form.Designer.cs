5685ba46fac59487d289bd4df4f64cde55c57578
Iman Eivazi <imaneivazi@gmail.com>
2025-12-24 13:22:23 -0800

    Initial commit: PhoneBook project setup

---
+++ PhoneBook/Forms/PhoneBook.Form.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace PhoneBook
+{
+    partial class PhoneBookForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.searchGroupBox = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.searchGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // searchGroupBox
+            // 
+            this.searchGroupBox.Controls.Add(this.textBox1);
+            this.searchGroupBox.Location = new System.Drawing.Point(13, 12);
+            this.searchGroupBox.Name = "searchGroupBox";
+            this.searchGroupBox.Size = new System.Drawing.Size(653, 80);
+            this.searchGroupBox.TabIndex = 0;
+            this.searchGroupBox.TabStop = false;
+            this.searchGroupBox.Text = "Search";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(641, 31);
+            this.textBox1.TabIndex = 0;
+            // 
+            // PhoneBookForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
+            this.ClientSize = new System.Drawing.Size(678, 544);
+            this.Controls.Add(this.searchGroupBox);
+            this.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.Font = new System.Drawing.Font("Mongolian Baiti", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "PhoneBookForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Phone Book";
+            this.searchGroupBox.ResumeLayout(false);
+            this.searchGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox searchGroupBox;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
