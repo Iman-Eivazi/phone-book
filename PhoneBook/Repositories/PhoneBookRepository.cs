db4988ba854b4cbc9fcfcdaff454843a29c81f78
Iman Eivazi <imaneivazi@gmail.com>
2025-12-24 14:25:45 -0800

    Get all contacts added in repo

--- PhoneBook/Repositories/PhoneBookRepository.cs
+++ PhoneBook/Repositories/PhoneBookRepository.cs
@@ -9,35 +9,37 @@ namespace PhoneBook.Repositories
     internal class PhoneBookRepository : IPhoneBookRepository
     {
         #region Variables
-
+        // This is the path to our data-base
         const string _connectionString = "Data Source = .; Initial Catalog = PhoneBookDb; Integrated Security = True;";
 
         #endregion
 
         public DataTable GetAllContacts()
         {
-            //string readQuery = ;
-
+            // It represent a sql connection, so we can use it to transfer datas.
             using (SqlConnection connection = new SqlConnection(_connectionString))
             {
+                // Somehow, it caries datas
                 SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Contact;", connection);
 
+                // Make a variable so we can keep our datas in program life-cycle
                 DataTable contactsList = new DataTable();
 
                 try
                 {
-                    adapter.Fill(contactsList);
+                    adapter.Fill(contactsList); // Fill recieved datas.
 
                     return contactsList;
                 }
                 catch (Exception ex)
                 {
+                    // If anything goes wrong, handle the error and show the error message.
                     MessageBox.Show($"Error loading contacts: {ex.Message}",
-                "Database Error",
-                MessageBoxButtons.OK,
-                MessageBoxIcon.Error);
+                                    "Database Error",
+                                    MessageBoxButtons.OK,
+                                    MessageBoxIcon.Error);
 
-                    return new DataTable();
+                    return new DataTable(); // return an empty table in DataTable type.
                 }
             }
         }
