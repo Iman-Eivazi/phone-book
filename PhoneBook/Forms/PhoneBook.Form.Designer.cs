834983e54b4ceda591a59b041ed9d191739a0855
Iman Eivazi <imaneivazi@gmail.com>
2025-12-24 14:14:49 -0800

    Add interface

--- PhoneBook/Forms/PhoneBook.Form.Designer.cs
+++ PhoneBook/Forms/PhoneBook.Form.Designer.cs
@@ -30,7 +30,12 @@
         {
             this.searchGroupBox = new System.Windows.Forms.GroupBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.contactsListGroupBox = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
             this.searchGroupBox.SuspendLayout();
+            this.contactsListGroupBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // searchGroupBox
@@ -50,12 +55,45 @@
             this.textBox1.Size = new System.Drawing.Size(641, 31);
             this.textBox1.TabIndex = 0;
             // 
+            // contactsListGroupBox
+            // 
+            this.contactsListGroupBox.Controls.Add(this.dataGridView1);
+            this.contactsListGroupBox.Location = new System.Drawing.Point(13, 98);
+            this.contactsListGroupBox.Name = "contactsListGroupBox";
+            this.contactsListGroupBox.Size = new System.Drawing.Size(653, 434);
+            this.contactsListGroupBox.TabIndex = 1;
+            this.contactsListGroupBox.TabStop = false;
+            this.contactsListGroupBox.Text = "Search";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 27);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(647, 404);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.toolStrip1.Location = new System.Drawing.Point(0, 563);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(678, 25);
+            this.toolStrip1.TabIndex = 2;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
             // PhoneBookForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
-            this.ClientSize = new System.Drawing.Size(678, 544);
+            this.ClientSize = new System.Drawing.Size(678, 588);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.contactsListGroupBox);
             this.Controls.Add(this.searchGroupBox);
             this.Cursor = System.Windows.Forms.Cursors.Cross;
             this.Font = new System.Drawing.Font("Mongolian Baiti", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -66,7 +104,10 @@
             this.Text = "Phone Book";
             this.searchGroupBox.ResumeLayout(false);
             this.searchGroupBox.PerformLayout();
+            this.contactsListGroupBox.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -74,6 +115,9 @@
 
         private System.Windows.Forms.GroupBox searchGroupBox;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox contactsListGroupBox;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
     }
 }
 
---
+++ PhoneBook/Repositories/PhoneBookRepository.cs
@@ -0,0 +1,45 @@
+﻿using PhoneBook.Interfaces;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace PhoneBook.Repositories
+{
+    internal class PhoneBookRepository : IPhoneBookRepository
+    {
+        #region Variables
+
+        const string _connectionString = "Data Source = .; Initial Catalog = PhoneBookDb; Integrated Security = True;";
+
+        #endregion
+
+        public DataTable GetAllContacts()
+        {
+            //string readQuery = ;
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Contact;", connection);
+
+                DataTable contactsList = new DataTable();
+
+                try
+                {
+                    adapter.Fill(contactsList);
+
+                    return contactsList;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error loading contacts: {ex.Message}",
+                "Database Error",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Error);
+
+                    return new DataTable();
+                }
+            }
+        }
+    }
+}
